7836d1e66b30b5435627ef1a450754f42765fac3
Alexander <alex@diveboard.com>
2016-05-17 05:50:18 -0700

    support early morning

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//Generic helper methods
 
 function contains(a, obj) {
     for (var i = 0; i < a.length; i++) {
@@ -57,8 +58,11 @@ var get_muni = function(request, response) {
           parseString(body, function (err, result) {
             var stopname = result.RTT.AgencyList[0].Agency[0].RouteList[0].Route[0].RouteDirectionList[0].RouteDirection[0].StopList[0].Stop[0].$.name;
             var stopcode= result.RTT.AgencyList[0].Agency[0].RouteList[0].Route[0].RouteDirectionList[0].RouteDirection[0].StopList[0].Stop[0].$.StopCode;
-
-            var leavetime = result.RTT.AgencyList[0].Agency[0].RouteList[0].Route[0].RouteDirectionList[0].RouteDirection[0].StopList[0].Stop[0].DepartureTimeList[0].DepartureTime[0];
+            try{
+              var leavetime = result.RTT.AgencyList[0].Agency[0].RouteList[0].Route[0].RouteDirectionList[0].RouteDirection[0].StopList[0].Stop[0].DepartureTimeList[0].DepartureTime[0];
+            }catch(e){
+              var leavetime = "unknown";
+            }
             stops[stopcode] = {name: stopname, time: leavetime};
             console.log(stops);
             if (!contains(Object.keys(stops).map(function(e){return stops[e];}), null)){
@@ -72,13 +76,56 @@ var get_muni = function(request, response) {
     }
 }
 
+
+//Start of the app server
+
+
+
+
+
+
+
+//Load required packages
 var express = require('express');
-var app = express();
 var wwwrequest = require('request');
 var parseString = require('xml2js').parseString;
+var mongoose = require('mongoose');
+var passport = require('passport');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+
+var userController = require('./controllers/user');
+var clientController = require('./controllers/client');
+var authController = require('./controllers/auth');
+var oauth2Controller = require('./controllers/oauth2');
+
+
+//Create db connection
+mongoose.connect('mongodb://alexksso:gn34ATk3rVm6@ds021922.mlab.com:21922/heroku_bqf311bg');
+
+// Create our Express application
+var app = express();
 
 
 app.set('port', (process.env.PORT || 5000));
+
+// Use the body-parser package in our application
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+// Use express session support since OAuth2orize requires it
+app.use(session({
+  secret: 'Super Secret Session Key',
+  saveUninitialized: true,
+  resave: true
+}));
+
+// Use the passport package in our application
+app.use(passport.initialize());
+
+// Create our Express router
+var router = express.Router();
 
 app.use(express.static(__dirname + '/public'));
 
@@ -86,9 +133,32 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.post('/', get_muni);
-app.get('/', get_muni);
+router.route('/')
+  .post(get_muni)
+  .get(get_muni);
 
+
+// Create endpoint handlers for /users
+router.route('/api/users')
+  .post(userController.postUsers)
+  .get(authController.isAuthenticated, userController.getUsers);
+
+// Create endpoint handlers for /clients
+router.route('/api/clients')
+  .post(authController.isAuthenticated, clientController.postClients)
+  .get(authController.isAuthenticated, clientController.getClients);
+
+// Create endpoint handlers for oauth2 authorize
+router.route('/oauth2/authorize')
+  .get(authController.isAuthenticated, oauth2Controller.authorization)
+  .post(authController.isAuthenticated, oauth2Controller.decision);
+
+// Create endpoint handlers for oauth2 token
+router.route('/oauth2/token')
+  .post(authController.isClientAuthenticated, oauth2Controller.token);
+
+// Register all our routes with /api
+app.use('/', router);
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
